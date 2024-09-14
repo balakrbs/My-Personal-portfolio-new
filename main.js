c4926c5eba3cf9946a30b6d5094f3dcc2761d296
balakrbs <balakrbs@gmail.com>
2024-09-14 17:27:52 +0530

    Media Queries and animation added

--- main.js
+++ main.js
@@ -64,14 +64,18 @@ themeBtn.addEventListener("click", () => {
   localStorage.setItem("saved-icon", getCurrentIcon());
 });
 
-const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
-const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+const getCurrentTheme = () =>
+  document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeBtn.classList.contains("sun") ? "sun" : "moon";
 
 const savedTheme = localStorage.getItem("saved-theme");
 const savedIcon = localStorage.getItem("saved-icon");
 
-if(savedTheme){
-  document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+if (savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+    "dark-theme"
+  );
   themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
 }
 
@@ -79,32 +83,131 @@ if(savedTheme){
 
 const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   scrollTopBtn.classList.toggle("active", window.scrollY > 500);
 });
 
-scrollTopBtn.addEventListener("click", () =>{
-  document.body.scrollTop=0;
-  document.documentElement.scrollTop=0;
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
- 
+
 //Navigation Menu items active on page scroll
 
-window.addEventListener("scroll", () =>{
+window.addEventListener("scroll", () => {
   const sections = document.querySelectorAll("section");
   const scrollY = window.pageYOffset;
 
-  sections.forEach(current =>{
+  sections.forEach((current) => {
     let sectionHeight = current.offsetHeight;
     let sectionTop = current.offsetTop - 50;
-    let id =current.getAttribute("id");
+    let id = current.getAttribute("id");
 
-    if(scrollY >sectionTop && scrollY <= sectionTop + sectionHeight){
-      document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
-    }
-    else{
-      document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav-items a[href*=" + id + "]")
+        .classList.remove("active");
     }
   });
+});
+
+//Responsive navigation menu toggle
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+  navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navigation.classList.remove("active");
+  });
+});
+
+//scroll reveal animations
+//common reveal options to create reveal animations
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 100,
+});
+
+//Target elements, and specify options to create reveal animations
+
+ScrollReveal().reveal(".home .info h2, .section-title-01, .section-title-02", {
+  delay: 500,
+  origin: "left"
+});
+ScrollReveal().reveal(".home .info h3, .home .info p, .about-info .btn, .theme-btn", {
+  delay: 600,
+  origin: "right",
+  interval: 200
+});
+
+ScrollReveal().reveal(".home .info .btn", {
+  delay: 700,
+  origin: "bottom",
+  interval: 300
+});
+
+ScrollReveal().reveal(".media-icons i, .contact-left li ", { delay: 500,
+  origin: "left",
+  interval: 200
+});
+
+ScrollReveal().reveal(".home-img, .about-img", {
+  delay: 500,
+  origin: "bottom"
+});
+
+ScrollReveal().reveal(".about .description, .copy-right", {
+  delay: 600,
+  origin: "right"
+});
+
+ScrollReveal().reveal(".about .professional-list li, .about .professional-list", {
+  delay: 500,
+  origin: "right",
+  interval: 200
+});
+
+ScrollReveal().reveal(".skills-description, .services-description, .contact-left h2", {
+  delay: 700,
+  origin: "right"
+});
+
+ScrollReveal().reveal(".contact-right p, input, textarea", {
+  delay: 700,
+  origin: "right",
+  interval:200
+});
+ScrollReveal().reveal(".contact-right p", {
+  delay: 700,
+  origin: "left",
+  interval:200
+});
+ScrollReveal().reveal(".experience-card, .service-card, .education, .protfolio, .img-card, .contact-form .btn, .contact-card", {
+  delay: 800,
+  origin: "bottom",
+  interval:200
+});
+
+ScrollReveal().reveal("footer .group, .follow-group, .footer-copyright, .scroll-down", {
+  delay: 900,
+  origin: "top",
+  interval:300
 });
 
